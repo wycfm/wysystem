8ed60ce564677a55c94fae542bc925565da130ed
wycfm <1361992823@qq.com>
2018-07-10 07:36:52 +0800

    update

--- src/main/java/cn/wycfm/core/filter/CoreFilter.java
+++ src/main/java/cn/wycfm/core/filter/CoreFilter.java
@@ -9,17 +9,21 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
+import cn.wycfm.core.service.UserService;
+import cn.wycfm.core.service.impl.UserServiceImpl;
+
 
 public class CoreFilter implements Filter{
 
+	private UserService userService;
 	
 	public void init(FilterConfig filterConfig) throws ServletException {
-		
+		userService = new UserServiceImpl();
 	}
 
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		
+		//userService
 		
 	}
 
