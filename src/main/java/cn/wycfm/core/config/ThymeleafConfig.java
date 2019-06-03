65a41da869e7a389e7de41c388693a4d352c8fa4
wycfm <1361992823@qq.com>
2019-06-03 22:52:40 +0800

    thymeleaf learn update

---
+++ src/main/java/cn/wycfm/core/config/ThymeleafConfig.java
@@ -0,0 +1,43 @@
+package cn.wycfm.core.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
+
+import cn.wycfm.core.util.TemplateEngineUtil;
+
+public class ThymeleafConfig implements ServletContextListener{
+
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void contextInitialized(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		TemplateEngine engine = templateEngine(sce.getServletContext());
+		TemplateEngineUtil.storeTemplateEngine(sce.getServletContext(), engine);
+	}
+	
+	private TemplateEngine templateEngine(ServletContext servletContext) {
+		TemplateEngine engine = new TemplateEngine();
+		engine.setTemplateResolver(templateResolver(servletContext));
+		return engine;
+	}
+	
+	
+	private ITemplateResolver templateResolver(ServletContext servletContext) {
+		ServletContextTemplateResolver resolver = new ServletContextTemplateResolver(servletContext);
+		resolver.setPrefix("/WEB-INF/templates/");
+		resolver.setCharacterEncoding("UTF-8");
+		resolver.setTemplateMode(TemplateMode.HTML);;
+		
+		return resolver;
+	}
+
+}
---
+++ src/main/java/cn/wycfm/core/util/TemplateEngineUtil.java
@@ -0,0 +1,27 @@
+package cn.wycfm.core.util;
+
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebListener;
+
+import org.thymeleaf.TemplateEngine;
+
+/**
+ * 将Thymeleaf TemplateEngine存储并检索到应用程序servlet上下文中。
+ * @author Administrator
+ *
+ */
+@WebListener
+public class TemplateEngineUtil {
+	
+	private static final String TEMPLATE_ENGINE_ATTR = "com.e-learn.thymeleaf3.TemplateEngineInstance";
+	
+	public static void storeTemplateEngine(ServletContext context, TemplateEngine engine) {
+        context.setAttribute(TEMPLATE_ENGINE_ATTR, engine);
+    }
+
+    public static TemplateEngine getTemplateEngine(ServletContext context) {
+        return (TemplateEngine) context.getAttribute(TEMPLATE_ENGINE_ATTR);
+    }
+
+
+}
